146659ba5fad7376be5d7991cf3846a866232a14
zmeriksen <zmeriksen@gmail.com>
2019-09-28 14:03:52 -0500

    Initial Commit

---
+++ List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  List
+//
+//  Created by Zach Eriksen on 9/28/19.
+//  Copyright © 2019 oneleif. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
